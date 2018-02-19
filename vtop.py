a0897f42b67288e021464550a35c3091509fdbb3
Trojan2498 <adityasinha2498@gmail.com>
2018-02-19 19:24:55 +0530

    academics option opening

--- vtop.py
+++ vtop.py
@@ -7,11 +7,15 @@ Created on Mon Feb 19 17:08:53 2018
 
 import os
 from selenium import webdriver
+from selenium.webdriver.support import ui
 from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
 
+def page_is_loaded(driver):
+    return driver.find_element_by_tag_name("body") != None
 
 executable_path = r"C:\Users\guest11\Documents\chromedriver\chromedriver.exe" #specify the path of chromedriver in your pc
-os.environ["webdriver.chrome.driver"] = executable_path
+#os.environ["webdriver.chrome.driver"] = executable_path
 
 chrome_options = Options()
 chrome_options.add_extension(r'C:\Users\guest11\AppData\Local\Google\Chrome\User Data\Default\Extensions\hafeeaangmkbibcaahfjdmmmeappjbbp\2.2_0.crx') #make a packed copy of this extension i.e. .crx format and specify its path
@@ -29,3 +33,13 @@ password_field.send_keys("Aditya@123")
 password_field.send_keys(Keys.RETURN)
 wait = ui.WebDriverWait(driver, 10)
 wait.until(page_is_loaded)
+print("hello")
+
+#vtopbeta_data = driver.page_source
+#soup_data = BeautifulSoup(vtopbeta_data,"html.parser")
+#print(soup_data)
+
+menu = driver.find_element_by_class_name("sidebar-toggle")
+menu.click()
+#academics = 
+driver.find_element_by_xpath("//div[@id='dbMenu']//ul//span[contains(.,'ACADEMICS')]").click()
